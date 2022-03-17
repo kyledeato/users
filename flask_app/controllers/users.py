b9a237712760dcf81fca885f38bf41332cdebe3c
Kyle Deato <kyledeato@gmail.com>
2022-03-16 23:39:12 -0700

    MVC format

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,53 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.user import User
+
+# DISPLAY ROUTES ------------
+@app.route('/')
+def index():
+    users = User.get_all()
+    print(users)
+    return render_template('index.html', users = users)
+
+@app.route('/create_user')
+def create_user():
+    return render_template('create.html')
+
+    # show
+@app.route('/user/<int:id>')
+def show_user(id):
+    data = {
+        "id" : id
+    }
+    user = User.get_one(data)
+    return render_template('show_user.html', user = user)
+
+@app.route('/user/create/edit/<int:id>')
+def edit_user(id):
+    user = User.get_one({"id": id })
+    return render_template('edit_user.html', user = user)
+
+# ACTION ROUTES -----------
+@app.route('/user/create', methods =['POST'])
+def create():
+    data = {
+        "fname": request.form["fname"],
+        "lname": request.form["lname"],
+        "email": request.form["email"]
+    }
+    print(request.form)
+    User.save(data)
+    return redirect('/')
+    
+    #update
+@app.route('/user/update', methods=['POST'])
+def update_user():
+    User.update_users(request.form)
+    print(request.form)
+    return redirect ('/')
+
+    #delete
+@app.route('/user/<int:id>/delete')
+def delete_user(id):
+    User.delete_user({"id": id})
+    return redirect('/')
--- server.py
+++ server.py
@@ -1,58 +1,8 @@
-from crypt import methods
-from flask import Flask, render_template, redirect, request
-from user import User
-app = Flask(__name__)
+from flask_app.controllers import users
+from flask_app.models.user import User
+from flask_app import app
 
 
-# DISPLAY ROUTES ------------
-@app.route('/')
-def index():
-    users = User.get_all()
-    print(users)
-    return render_template('index.html', users = users)
-
-@app.route('/create_user')
-def create_user():
-    return render_template('create.html')
-
-    # show
-@app.route('/user/<int:id>')
-def show_user(id):
-    data = {
-        "id" : id
-    }
-    user = User.get_one(data)
-    return render_template('show_user.html', user = user)
-
-@app.route('/user/create/edit/<int:id>')
-def edit_user(id):
-    user = User.get_one({"id": id })
-    return render_template('edit_user.html', user = user)
-
-# ACTION ROUTES -----------
-@app.route('/user/create', methods =['POST'])
-def create():
-    data = {
-        "fname": request.form["fname"],
-        "lname": request.form["lname"],
-        "email": request.form["email"]
-    }
-    print(request.form)
-    User.save(data)
-    return redirect('/')
-    
-    #update
-@app.route('/user/update', methods=['POST'])
-def update_user():
-    User.update_users(request.form)
-    print(request.form)
-    return redirect ('/')
-
-    #delete
-@app.route('/user/<int:id>/delete')
-def delete_user(id):
-    User.delete_user({"id": id})
-    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
